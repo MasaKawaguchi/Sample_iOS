f3098e448ba3afee89a77b23b7e2c6de3b9ab1af
masaKawaguchi <kawaguchimas@citizen.co.jp>
2020-09-17 19:20:32 +0900

    Unitテストが失敗する状態にしてcommit

--- sample/sampleTests/sampleTests.swift
+++ sample/sampleTests/sampleTests.swift
@@ -33,7 +33,9 @@ class sampleTests: XCTestCase {
     func testMySample() {
         let mySample = MySample()
         let add = mySample.add(a: 2, b: 3)
-        // addメソッドの返り値が5であるかの判断
-        XCTAssertEqual(add, 5)
+        // addメソッドの戻り値が5であるかの判断(Unitテスト成功例)
+//        XCTAssertEqual(add, 5)
+        // addメソッドの戻り値は5だが、あえて5以外の値でチェックし、unitテストエラーにさせる(Unitテスト失敗例)
+        XCTAssertEqual(add, 1)
     }
 }
